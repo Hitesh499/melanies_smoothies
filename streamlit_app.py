8fedadb8d28e9e9a30c6620506e1786d464444d1
Hitesh499 <hiteshdarshan136@gmail.com>
2026-05-05 22:38:36 +0530

    Refactor smoothie app for improved code clarity

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import requests
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
+# Input for Name on Smoothie
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
@@ -14,11 +15,11 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# --- FIX 1: Select BOTH columns so Pandas can find SEARCH_ON later ---
+# Get the fruit options including the SEARCH_ON column
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 
-# --- FIX 2: Use the Pandas column for the multiselect options ---
+# Multiselect for ingredients
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     pd_df['FRUIT_NAME'],
@@ -31,20 +32,22 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        # Pull the search value from our dataframe
+        # Pull the SEARCH_ON value
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        st.write(f'The search value for {fruit_chosen} is {search_on}.')
 
-        st.subheader(fruit_chosen + ' Nutrition Information')
+        # Display Nutrition subheader
+        st.subheader(f'{fruit_chosen} Nutrition Information')
         
-        # --- FIX 3: Use the dynamic SEARCH_ON value for the API call ---
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        # API Call using f-string (The modern, clean way!)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Build the SQL Insert Statement
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
+    # Submit button logic
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
